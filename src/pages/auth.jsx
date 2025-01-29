1d93836ef99dad3c3ef2a325b4c9b709962f8b33
Keshav Prajapati <keshavpj1711@gmail.com>
2025-01-30 04:24:06 +0530

    update: imported the login and signup component

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -1,6 +1,27 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import Login from "../components/login"
+import SignUp from "../components/signup"
+
+
 const AuthenticationPage = () => {
   return (
-    <div className="p-10 text-center mt-10">AuthenticationPage</div>
+    <div className="mt-36 flex flex-col items-center gap-10">
+      <h1 className="text-5xl font-extrabold">Login/SignUp</h1>
+
+      <Tabs defaultValue="Login" className="w-[400px]">
+        <TabsList className='w-full grid grid-cols-2'>
+          <TabsTrigger value="Login" className="grow">Login</TabsTrigger>
+          <TabsTrigger value="Signup" className="grow">SignUp</TabsTrigger>
+        </TabsList>
+        <TabsContent value="Login">
+          <Login />
+        </TabsContent>
+        <TabsContent value="Signup">
+          <SignUp />
+        </TabsContent>
+      </Tabs>
+
+    </div>
   )
 }
 
