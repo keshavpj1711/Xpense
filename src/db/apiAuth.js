3cbf187f5e199b9468af5e45e2cc296688ac9fb7
Keshav Prajapati <keshavpj1711@gmail.com>
2025-02-01 21:00:18 +0530

    update: added the getCurrentUser() function which tells the user is added or not

--- src/db/apiAuth.js
+++ src/db/apiAuth.js
@@ -1,25 +1,37 @@
 import supabase from "./supabase";
 
-export async function login({email, password}) {
-  const {data, error} = await supabase.auth.signInWithPassword({
-    email, 
-    password,
-  });
-
-  if(error) throw new Error(error.message);
-
-  return data;
-}
-
-export async function signup({userName, email, password}){
-  const { data, error } = await supabase.auth.signUp({
-    userName, 
+export async function login({ email, password }) {
+  const { data, error } = await supabase.auth.signInWithPassword({
     email,
     password,
   });
 
-  if(error) throw new Error(error.message);
+  if (error) throw new Error(error.message);
 
   return data;
 }
-  
+
+export async function signup({ userName, email, password }) {
+  const { data, error } = await supabase.auth.signUp({
+    userName,
+    email,
+    password,
+  });
+
+  if (error) throw new Error(error.message);
+
+  return data;
+}
+
+export async function getCurrentUser() {
+  // To get the session information of the user stored in the local storage
+  const { data: session, error } = await supabase.auth.getSession();
+
+  if (!session.session) {
+    return null;
+  }
+  if (error) {
+    throw new Error(error.message);
+  }
+  return session.session?.user; // Returning the user session info if it exists
+}
