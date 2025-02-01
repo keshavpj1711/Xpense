52a83f71a098a2781a8613fee8c711a7bb828347
Keshav Prajapati <keshavpj1711@gmail.com>
2025-02-01 22:31:10 +0530

    fix: after login user can't access the /auth page

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -1,9 +1,25 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import Login from "../components/login"
 import SignUp from "../components/signup"
+import { useNavigate, useSearchParams } from "react-router-dom"
+import { UrlState } from "../context"
+import { useEffect } from "react"
 
 
 const AuthenticationPage = () => {
+  // So when the user is logged in so it shouldn't be able to go to the auth page so we need to work on that
+  const navigate = useNavigate();
+
+  const {isAuthenticated, loading} = UrlState();
+
+  // Navigating the user back to dashboard if authenticated
+  useEffect(() => {
+    if (isAuthenticated && !loading) {
+      navigate('/dashboard')
+    }
+  }, [isAuthenticated, loading]) // this keeps in mind that they are the dependecies for for this navigation 
+
+
   return (
     <div className="mt-36 flex flex-col items-center gap-10">
       <h1 className="text-5xl font-extrabold">Login/SignUp</h1>
